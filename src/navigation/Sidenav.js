f35204b050d9784e219d97e3bbbb7412b6c5163f
asanibrahim <asanib98@gmail.com>
2024-08-20 00:10:26 +0530

    add icons

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React from "react";
 import "./Sidenav.css";
-import HomeIcon from '@mui/icons-material/Home';
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import ExploreIcon from "@mui/icons-material/Explore";
+import ChatIcon from "@mui/icons-material/Chat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Sidenav() {
   return (
-    <div className='sidenav'>
-        <img className='sidenav__logo' src='https://www.pngkey.com/png/full/828-8286178_mackey-work-needs-no-elaborate-presentation-or-distracting.png'
-        alt=''/>
-        <div className='sidenav__button'>
-            <div  className='side__button'>
-                {/* {icons} */}
-                <span>Home</span>
-            </div>
-        </div>
+    <div className="sidenav">
+      <img
+        className="sidenav__logo"
+        src="https://www.pngkey.com/png/full/828-8286178_mackey-work-needs-no-elaborate-presentation-or-distracting.png"
+        alt=""
+      />
+      <div className="sidenav__button">
+        <button className="side__button">
+          <HomeIcon />
+          <span>Home</span>
+        </button>
+        <button className="side__button">
+          <SearchIcon />
+          <span>Search</span>
+        </button>
+        <button className="side__button">
+          <ExploreIcon />
+          <span>Explore</span>
+        </button>
+        <button className="side__button">
+          <SlideshowIcon />
+          <span>Reels</span>
+        </button>
+        <button className="side__button">
+          <ChatIcon />
+          <span>Message</span>
+        </button>
+        <button className="side__button">
+          <FavoriteBorderIcon />
+          <span>Notification</span>
+        </button>
+        <button className="side__button">
+          <AddCircleIcon />
+          <span>Create</span>
+        </button>
+        <button className="side__button">
+          <MenuIcon />
+          <span>Menu</span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidenav 
+export default Sidenav;
