dce54e272fb317a573bed26396379f03e1ce06b0
asanibrahim <asanib98@gmail.com>
2024-08-21 23:30:53 +0530

    create new feed

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Time.css";
 import Suggesstions from "./Suggesstion";
+import Post from "./post/Post";
 
 function Timeline() {
   return (
     <div className="timeline">
-      <div className="timeline__left">Timelines</div>
+      <div className="timeline__left">
+        <div className="timeline_posts">
+          <Post/>
+          <Post/>
+          <Post/>
+        </div>
+      </div>
       <div className="timeline__right">
         <Suggesstions/>
       </div>
