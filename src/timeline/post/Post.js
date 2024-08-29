59e6665b62018e400650c838fbef386ad2a6810c
asanibrahim <asanib98@gmail.com>
2024-08-29 22:28:11 +0530

    add menu

--- src/timeline/post/Post.js
+++ src/timeline/post/Post.js
@@ -30,7 +30,7 @@ function Post({user,postImage,likes,timepamp}) {
             <Bookmark className='postIcon'/>
          </div>
         </div>
-       Liked by {likes} peoples
+       Liked by {likes} people
     </div>
     </div>
   )
